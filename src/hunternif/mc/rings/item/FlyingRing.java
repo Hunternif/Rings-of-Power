a012645f130c8439e74bf08412f8114f5391724b
Hunternif <hunternif@gmail.com>
2013-11-03 17:22:14 +0400

    Fixed Flying ring for multiplayer

--- src/hunternif/mc/rings/item/FlyingRing.java
+++ src/hunternif/mc/rings/item/FlyingRing.java
@@ -1,58 +1,70 @@
 package hunternif.mc.rings.item;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 
 public class FlyingRing extends PoweredRing {
+	private static final String TAG_FUEL_UNIT_CONSUMPTION = "RoPFuelUnitCons";
+	
 	public FlyingRing(int id) {
 		super(id);
 		MinecraftForge.EVENT_BUS.register(this);
 	}
 	
-	private long ticksFlying = 0;
-	private boolean hadThisItemLastTime = false;
+	@Override
+	public void onUpdate(ItemStack stack, World world, Entity entity, int slot, boolean isEquipped) {
+		if (entity instanceof EntityPlayer && !entity.worldObj.isRemote) {
+			EntityPlayer player = (EntityPlayer) entity;
+			if (player.capabilities.isCreativeMode) {
+				return;
+			}
+			if (hasFuel(stack, player)) {
+				allowFlying(player);
+			} else {
+				forbidFlying(player);
+			}
+			if (player.capabilities.isFlying) {
+				int fuelTicks = getFuelUnitTicks(stack);
+				if (fuelTicks % 100 == 0) { // 20 ticks * 5 seconds
+					consumeFuel(stack, player);
+				}
+				incrementFuelUnitTicks(stack);
+			}
+		}
+	}
+	
+	private Set<EntityPlayer> playersWithThisItem = Collections.synchronizedSet(new HashSet<EntityPlayer>());
+	
 	@ForgeSubscribe
 	public void onPlayerUpdate(LivingUpdateEvent event) {
 		if (event.entityLiving instanceof EntityPlayer && !event.entity.worldObj.isRemote) {
 			EntityPlayer player = (EntityPlayer) event.entityLiving;
+			if (player.capabilities.isCreativeMode) {
+				return;
+			}
 			ItemStack stack = findThisItem(player.inventory);
 			if (stack != null) {
-				if (!hadThisItemLastTime) {
-					hadThisItemLastTime = true;
+				if (!playersWithThisItem.contains(player)) {
+					playersWithThisItem.add(player);
 					if (hasFuel(stack, player)) {
-						player.capabilities.allowFlying = true;
-						player.sendPlayerAbilities();
+						allowFlying(player);
 					}
-				}
-				// All fuel consumed
-				if (!hasFuel(stack, player) && !player.capabilities.isCreativeMode) {
-					player.capabilities.allowFlying = false;
-					player.capabilities.isFlying = false;
-					player.sendPlayerAbilities();
-					ticksFlying = 0;
-				} else if (!player.capabilities.allowFlying) {
-					player.capabilities.allowFlying = true;
-					player.sendPlayerAbilities();
-				}
-				if (player.capabilities.isFlying && !player.capabilities.isCreativeMode) {
-					if (ticksFlying % 100 == 0) { // 20 ticks * 5 seconds
-						consumeFuel(stack, player);
-					}
-					ticksFlying++;
 				}
 			} else {
-				if (hadThisItemLastTime) {
-					ticksFlying = 0;
-					if (!player.capabilities.isCreativeMode) {
-						player.capabilities.allowFlying = false;
-						player.capabilities.isFlying = false;
-						player.sendPlayerAbilities();
-					}
-					hadThisItemLastTime = false;
+				if (playersWithThisItem.contains(player)) {
+					forbidFlying(player);
+					playersWithThisItem.remove(player);
 				}
 			}
 		}
@@ -60,7 +72,7 @@ public class FlyingRing extends PoweredRing {
 	
 	@Override
 	public boolean hasFuel(ItemStack stack, EntityPlayer player) {
-		return super.hasFuel(stack, player) || ticksFlying % 100 != 0;
+		return super.hasFuel(stack, player) || getFuelUnitTicks(stack) % 100 != 0;
 	}
 	
 	protected ItemStack findThisItem(IInventory inventory) {
@@ -71,5 +83,45 @@ public class FlyingRing extends PoweredRing {
 			}
 		}
 		return null;
+	}
+	
+	protected void allowFlying(EntityPlayer player) {
+		if (!player.capabilities.allowFlying) {
+			player.capabilities.allowFlying = true;
+			player.sendPlayerAbilities();
+		}
+	}
+	protected void forbidFlying(EntityPlayer player) {
+		if (player.capabilities.allowFlying) {
+			player.capabilities.isFlying = false;
+			player.capabilities.allowFlying = false;
+			player.sendPlayerAbilities();
+		}
+	}
+	
+	/** Number of ticks consuming since last consumption of fuel. */
+	public int getFuelUnitTicks(ItemStack stack) {
+		NBTTagCompound tag = stack.getTagCompound();
+		if (tag == null) {
+			return 0;
+		}
+		return tag.getInteger(TAG_FUEL_UNIT_CONSUMPTION);
+	}
+	
+	protected void incrementFuelUnitTicks(ItemStack stack) {
+		NBTTagCompound tag = stack.getTagCompound();
+		if (tag == null) {
+			tag = new NBTTagCompound();
+			stack.setTagCompound(tag);
+		}
+		int curTicks = getFuelUnitTicks(stack);
+		tag.setInteger(TAG_FUEL_UNIT_CONSUMPTION, curTicks + 1);
+	}
+	
+	@Override
+	public void consumeFuel(ItemStack stack, EntityPlayer player) {
+		super.consumeFuel(stack, player);
+		NBTTagCompound tag = stack.getTagCompound();
+		tag.setInteger(TAG_FUEL_UNIT_CONSUMPTION, 0);
 	}
 }
